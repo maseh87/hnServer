7306be4182c45a700db86fe4ece59760eb582fae
Mason <maseh87@gmail.com>
2014-07-07 21:19:06 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var app = express();
+var q = require('q');
+var request = require('request');
+var cors = require('cors');
+var cheerio = require('cheerio');
+
+var morgan = require('morgan');
+
+app.use(cors());
+app.use(morgan('dev'));
+
+var options = {
+  url: "https://community-hnify.p.mashape.com/get/best",
+  headers: {
+    "X-Mashape-Key": "ezm6j6ZDTTmshfYpcmqam21hJaXGp1taozKjsnkZZpn9dmHahi"
+  }
+};
+var cache = [];
+app.get('/articles', function(req, res) {
+  //get articles from mashape
+  request(options, function(err, response, body) {
+    if(err) {
+      return res.send(500);
+    }
+    var body = JSON.parse(body);
+    request(body.stories[0].link, function(storyError, storyResponse, storyBody) {
+      if(storyError) {
+        return res.send(500);
+      }
+      var $ = cheerio.load(storyBody);
+      cache.push($('body').html());
+      res.send(200);
+    })
+  })
+  //pre fetch articles
+
+  // res.send(200);
+});
+
+app.get('/test', function(req, res) {
+  res.set('Content-type', 'text/html');
+  res.send(cache[0]);
+});
+
+
+app.listen(3000);
+console.log('App is listening');
+
